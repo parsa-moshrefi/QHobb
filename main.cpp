66964f68bb21c1bd31eba7286b48624ec16ad249
Parsa Moshrefi <parsa.moshrefi@gmail.com>
2025-05-31 10:57:41 +0330

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <omp.h>
+#include "constants/literals.h"
+#include "glutils/windowUtil.h"
+#include "screen/screenLayout.h"
+
+/**************************
+ * WinMain
+ *
+ **************************/
+
+int WINAPI WinMain (HINSTANCE hInstance,
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpCmdLine,
+                    int iCmdShow)
+{
+    WNDCLASS wc;
+    HWND hWnd;
+    HDC hDC;
+    HGLRC hRC;
+    bQuit = FALSE;
+	
+	/* set number of threads on omp; Although opengl internally creates
+	   additional threads. */
+	char ompEnv[] = "OMP_NUM_THREADS=1";
+	putenv(ompEnv);
+	registerWindow(wc, hInstance);
+
+    /* create main window */    
+    hWnd = CreateWindow (__lpClassName, __lpWindowName, 
+	  WS_CAPTION | WS_POPUPWINDOW | WS_VISIBLE, 
+	  windowX, windowY, windowNWidth, windowNHeight, 
+	  NULL, NULL, hInstance, NULL);
+
+    /* enable OpenGL for the window */
+    EnableOpenGL (hWnd, &hDC, &hRC);
+	initScreenVals(heights, colors, marked, closures);
+	setScreenColors(heights, colors);
+	renderScreen();
+	
+	/* shutdown OpenGL */
+    DisableOpenGL (hWnd, hDC, hRC);
+
+    /* destroy the window explicitly */
+    DestroyWindow (hWnd);
+
+    return msg.wParam;
+}
