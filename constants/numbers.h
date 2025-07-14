cf386fa0b833a763de0878fea8282e4c9a432682
Parsa Moshrefi <parsa.moshrefi@gmail.com>
2025-07-14 08:53:33 +0330

    Update numbers.h

--- constants/numbers.h
+++ constants/numbers.h
@@ -1,71 +1,104 @@
-#ifndef __NUMBERS__
-#define __NUMBERS__
-#include <random>
-#include <wingdi.h>
-using namespace std;
+#ifndef __COORDUTILS__
+#define __COORDUTILS__
+#include "../constants/numbers.h"
+#include "../constants/types.h"
+#include "../glutils/libUtils.h"
 
-enum Color { PURPLE = 1, YELLOW = 2, RED = 3, GREEN = 4, BLUE = 5, DARK = 6, WHITE = 7 };
+void calculateCoords(short x, short y, float &vx1, float &vx2, float &vy1, float &vy2) {
+	short xCoeff = x - (rowBoxNumbers / 2);
+	short yCoeff = y - (columnBoxNumbers / 2);
+	vx1 = xCoeff * (sideHorizontalLength + rubberHThickness);
+	vy1 = yCoeff * (sideVerticalLength + rubberVThickness);
+	vx2 = xCoeff * (sideHorizontalLength + rubberHThickness) + sideHorizontalLength;
+	vy2 = yCoeff * (sideVerticalLength + rubberVThickness) + sideVerticalLength;
+}
 
-const short zero = 0;
-const short windowX = 600;
-const short windowY = 0;
-const short windowNWidth = 768;
-const short windowNHeight = 1024;
-const short rowBoxNumbers = 8;
-const short columnBoxNumbers = 12;
-const short maxClosuresNumber = 10;
-const short darkScore = 0;
-const short purpleScore = 2;
-const short yellowScore = 4;
-const short redScore = 6;
-const short greenScore = 8;
-const short blueScore = 10;
-const short closureMinVal = 3;
-const short blinkTimes = 5;
-const short blinkDuration = 200;
-const float initialTheta = 0.0f;
-const float purpleR = 1.0f;
-const float purpleG = 0.0f;
-const float purpleB = 1.0f;
-const float yellowR = 1.0f;
-const float yellowG = 1.0f;
-const float yellowB = 0.0f;
-const float redR = 1.0f;
-const float redG = 0.0f;
-const float redB = 0.0f;
-const float greenR = 0.0f;
-const float greenG = 1.0f;
-const float greenB = 0.0f;
-const float blueR = 0.0f;
-const float blueG = 0.0f;
-const float blueB = 1.0f;
-const float darkR = 0.0f;
-const float darkG = 0.0f;
-const float darkB = 0.0f;
-const float whiteR = 1.0f;
-const float whiteG = 1.0f;
-const float whiteB = 1.0f;
-const float rubberHThickness = 0.04f;
-const float sideVerticalLength = 0.145f;
-const float rubberVThickness = 0.02f;
-const float sideHorizontalLength = 0.205f;
-const float scorePerBoxVanish = 5.0f;
-const float winScore = 300.0f;
-const unsigned int winningScore = 100;
-const UINT swapFlag = WGL_SWAP_MAIN_PLANE | WGL_SWAP_OVERLAY1 | WGL_SWAP_OVERLAY2 |	WGL_SWAP_OVERLAY3 | WGL_SWAP_OVERLAY4 | WGL_SWAP_OVERLAY5 |
-	WGL_SWAP_OVERLAY6 | WGL_SWAP_OVERLAY7 | WGL_SWAP_OVERLAY8 |	WGL_SWAP_OVERLAY9 | WGL_SWAP_OVERLAY10 | WGL_SWAP_OVERLAY11 | WGL_SWAP_OVERLAY12 | 
-	WGL_SWAP_OVERLAY13 | WGL_SWAP_OVERLAY14 | WGL_SWAP_OVERLAY15 | WGL_SWAP_UNDERLAY1 | WGL_SWAP_UNDERLAY2 | WGL_SWAP_UNDERLAY3 | WGL_SWAP_UNDERLAY4 | 
-	WGL_SWAP_UNDERLAY5 | WGL_SWAP_UNDERLAY6 | WGL_SWAP_UNDERLAY7 | WGL_SWAP_UNDERLAY8 |	WGL_SWAP_UNDERLAY9 | WGL_SWAP_UNDERLAY10 | 
-	WGL_SWAP_UNDERLAY11 | WGL_SWAP_UNDERLAY12 | WGL_SWAP_UNDERLAY13 | WGL_SWAP_UNDERLAY14 |	WGL_SWAP_UNDERLAY15;
+void setUpTriangleCoords(float &vx1, float &vx2, float &vx3, float &vy1, float &vy2, float &vy3) {
+	vx1 = -(columnBoxNumbers/8) * (sideHorizontalLength + rubberHThickness);
+	vx2 = -vx1;
+	vx3 = 0.0f;
+	vy1 = vy2 = -(rowBoxNumbers/8) * (sideVerticalLength + rubberHThickness);
+	vy3 = -vy1;
+}
 
-const Color rubberColor = WHITE;
+void setUpsideTriangleCoords(float &vx1, float &vx2, float &vx3, float &vy1, float &vy2, float &vy3) {
+	vx1 = -(columnBoxNumbers/8) * (sideHorizontalLength + rubberHThickness);
+	vx2 = -vx1;
+	vx3 = 0.0f;
+	vy1 = vy2 = (rowBoxNumbers/8) * (sideVerticalLength + rubberHThickness);
+	vy3 = -vy1;
+}
 
-int getRandomNumber(int min, int max) {
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<> distrib(min, max);
-	int randomValue = distrib(gen);
-	return randomValue;
+// Written in order to use with glCopyPixels(GLint x, GLint y, GLsizei width, GLsizei height, GLenum type);
+pair<float, float> getOrigin() {
+	float ox = -(rowBoxNumbers/2) * (sideHorizontalLength+rubberHThickness);
+	float oy = -(columnBoxNumbers/2) * (sideVerticalLength+rubberVThickness);
+	return pair<float, float>(ox, oy);
+}
+
+bool darkFilled(pair<short, short> coord) {
+	if (coord.first == -1 || coord.second == -1)
+		return true;
+	
+	return false;
+}
+
+bool foundToVanish(pair<short, short>* coords) {
+	for (short i=0; i<closureMinVal; i++) {
+		if (darkFilled(coords[i]))
+			return false;
+	}
+	
+	return true;
+}
+
+void freeResources() {
+	short i;
+	for (i = 0; i<rowBoxNumbers; i++) {
+		free(colors[i]);
+		free(marked[i]);
+	}
+	
+	free(heights);
+	free(colors);
+	free(marked);
+	free(closures);
+}
+
+short min(short a, short b) {
+	return (a < b) ? a : b;
+}
+
+short max(short a, short b) {
+	return (a > b) ? a : b;
+}
+
+/***************************************************************************************************************
+ * In the next 6 functions rs[x|y][1|2] are rubber segment x or y pixel coordination of two diameter endpoints *
+ ***************************************************************************************************************/
+
+void coords_ld_Rbbr(float vx1, float vy1, float& rrx1, float &rry1, float &rrx2, float &rry2) {
+	// todo: impl
+}
+
+void coords_lu_Rbbr(float vx1, float vy2, float &rrx1, float &rry1, float &rrx2, float &rry2) {
+	// todo: impl
+}
+
+void coords_rd_Rbbr(float vx2, float vy1, float &rrx1, float &rry1, float &rrx2, float &rry2)  {
+	// todo: impl
+}
+
+void coords_ru_Rbbr(float vx2, float vy2, float &rrx1, float &rry1, float &rrx2, float &rry2) {
+	// todo: impl
+}
+
+void coordsLeftRbbr(float vx1, float vy1, float vy2, float &rrx1, float rry1, float &rrx2, float &rry2) {
+	// todo: impl
+}
+
+void coordsRightRbbr(float vx2, float vy1, float vy2, float &rrx1, float &rry1, float &rrx2, float &rry2) {
+	// todo: impl
 }
 
 #endif
