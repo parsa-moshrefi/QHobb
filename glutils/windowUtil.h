03b5a35f712a3962dc3bd57ac34e681ec55fe349
Parsa Moshrefi <parsa.moshrefi@gmail.com>
2025-09-09 09:27:55 +0330

    Update windowUtil.h

--- glutils/windowUtil.h
+++ glutils/windowUtil.h
@@ -2,6 +2,7 @@
 #define __WINDOWUTIL__
 #include "../constants/literals.h"
 #include "libUtils.h"
+#include "../glutils/basicShapes.h"
 
 void registerWindow(WNDCLASS& wc, HINSTANCE& hInstance) {
 	/* register window class */
@@ -16,6 +17,144 @@ void registerWindow(WNDCLASS& wc, HINSTANCE& hInstance) {
 	wc.lpszMenuName = NULL;
 	wc.lpszClassName = __lpClassName;
 	RegisterClass(&wc);
+}
+
+void checkDirection(MSG &msg) {
+	rubberMoved = true;
+	
+	if (msg.message != amsg1) {
+		direction = NONE;
+		rubberMoved = false;
+		return;
+	}
+	
+	switch (msg.wParam) {
+		case lawParam:
+			direction = LEFT;
+			break;
+			
+		case uawParam:
+			direction = UP;
+			break;
+
+		case rawParam:
+			direction = RIGHT;
+			break;
+			
+		case dawParam:
+			direction = DOWN;
+			break;
+			
+		default:
+			direction = NONE;
+			rubberMoved = false;
+			break;
+	}
+}
+
+void checkSwap(MSG &msg) {
+	if (msg.message != wsMsg1) {
+		doSwap = false;
+		return;
+	}
+	
+	if (msg.wParam == wswParam) {
+		doSwap = true;
+	}
+}
+
+void kbQHBFunc() {
+	checkDirection(msg);
+	checkSwap(msg);
+}
+
+void clearKBStat() {
+	direction = NONE;
+	doSwap = false;
+	rubberMoved = false;
+}
+
+void moveLeft() {
+	if (rbbrx <= 0)
+		return;
+		
+	rbbrx--;
+	direction = NONE;
+}
+
+void moveUp() {
+	if (rbbry >= columnBoxNumbers - 1)
+		return;
+		
+	rbbry++;
+	direction = NONE;
+}
+
+void moveRight() {
+	if (rbbrx >= rowBoxNumbers - 2)
+		return;
+		
+	rbbrx++;
+	direction = NONE;
+}
+
+void moveDown() {
+	if (rbbry <= 0)
+		return;
+		
+	rbbry--;
+	direction = NONE;
+}
+
+void moveRubber() {
+	if (direction == NONE)
+		return;
+	
+	eraseRubber();
+	switch (direction) {
+		case LEFT:
+			moveLeft();
+			break;
+			
+		case UP:
+			moveUp();
+			break;
+			
+		case RIGHT:
+			moveRight();
+			break;
+			
+		case DOWN:
+			moveDown();
+			break;
+		
+		default:
+			break;
+	}
+}
+
+bool checkSurrounding() {
+	return rbbrx >= 0 && rbbrx <= rowBoxNumbers - 2 && rbbry >= 0 && rbbry <= columnBoxNumbers - 1;
+}
+
+bool checkDarkness() {
+	return colors[rbbrx][rbbry] == DARK || colors[rbbrx + 1][rbbry] == DARK;
+}
+
+void swapRubberElems() {
+	if (!doSwap || !checkSurrounding() || checkDarkness())
+		return;
+	
+	Color left = colors[rbbrx][rbbry], right = colors[rbbrx + 1][rbbry];
+	drawBox(rbbrx, rbbry, right);
+	drawBox(rbbrx + 1, rbbry, left);
+	colors[rbbrx + 1][rbbry] = left;
+	colors[rbbrx][rbbry] = right;
+}
+
+void doActions() {
+	moveRubber();
+	swapRubberElems();
 }
 
 #endif
